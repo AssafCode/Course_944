7078aaf8f3a965a8ade0065b31bc7917b2165067
944 <944@stddom.sela>
2018-08-06 12:05:33 +0300

    2018-08-06_1205 PM

---
+++ WCF/03 - Service Reference Client/WcfService1/ServiceReferenceClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceReferenceClient.RemoteServerFullService;
+using ServiceReferenceClient.RemoteServerMinimizedService;
+
+namespace ServiceReferenceClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RemoteServerFullService.FullServiceClient serverConnectorFull = new RemoteServerFullService.FullServiceClient();
+            string resultFull = serverConnectorFull.GetData(6);
+            Console.WriteLine(resultFull);
+
+            RemoteServerMinimizedService.ServiceMinimizedClient serverConnectorMinimized = new RemoteServerMinimizedService.ServiceMinimizedClient();
+            string resultMini = serverConnectorMinimized.GetDataMinimized(6);
+            Console.WriteLine(resultMini);
+        }
+    }
+}
---
+++ WCF/04 - basicHttpBinding Client/WcfService1/MyHttpClient/Program.cs
@@ -0,0 +1,45 @@
+﻿using mini = ServiceReferenceClient.RemoteServerMinimizedService;
+using full = ServiceReferenceClient.RemoteServerFullService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHttpClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This code is written by an application developer.
+            // Create a channel factory.
+            BasicHttpBinding myBinding = new BasicHttpBinding();
+
+            EndpointAddress myEndpointFull = new EndpointAddress("http://localhost:58151/Service1.svc/Regular");
+
+            ChannelFactory<full.IFullService> myChannelFactory = new ChannelFactory<full.IFullService>(myBinding, myEndpointFull);
+
+            // Create a channel.
+            full.IFullService channelFull = myChannelFactory.CreateChannel();
+            string s = channelFull.GetData(5);
+            Console.WriteLine(s.ToString());
+            ((IClientChannel)channelFull).Close();
+
+            
+
+            
+
+            EndpointAddress myEndpointMini = new EndpointAddress("http://localhost:58151/Service1.svc/Minimized");
+
+            ChannelFactory<mini.IServiceMinimized> myChannelFactoryMini = new ChannelFactory<mini.IServiceMinimized>(myBinding, myEndpointMini);
+
+            // Create a channel.
+            mini.IServiceMinimized channelMini = myChannelFactoryMini.CreateChannel();
+            string sMini = channelMini.GetDataMinimized(7);
+            Console.WriteLine(sMini.ToString());
+            ((IClientChannel)channelMini).Close();
+        }
+    }
+}
