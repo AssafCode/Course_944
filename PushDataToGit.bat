2fca9a5f81a61e436553a7d559215f1f4870d2d1
944 <944@stddom.sela>
2018-08-06 14:34:25 +0300

    2018-08-06_0234 PM

--- PushDataToGit.bat
+++ PushDataToGit.bat
@@ -1,11 +1,3 @@
-@echo off
-For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
-For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
-@echo on
+
 cd ../
 xcopy /b /s *.* Course_944
-CD Course_944
-git add *
-git commit -m "%mydate%_%mytime%"
-git push
-pause
