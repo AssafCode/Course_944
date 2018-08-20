bb8eed4973354cc3d3dff9e15be11f93dd65ab47
944 <944@stddom.sela>
2018-08-20 16:26:52 +0300

    wpf

---
+++ Windows Client Side Win 10/944-WPF-Uriel/Demos/Custom messgebox/Wpf_Windows/Message.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Wpf_Windows
+{
+    //custom message box
+    public partial class Message : Window
+    {
+        private Message()
+        {
+            InitializeComponent();
+        }
+
+        private static Message _insta;
+        private static Message insta
+        {
+            get
+            {
+                if (_insta == null)
+                    _insta = new Message();
+                return _insta;
+            }
+        }
+
+        // custom function Show
+        public static void Show(string msg)
+        {
+            insta.text.Text = msg;
+            //use windows. Show
+            insta.ShowDialog();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
---
+++ Windows Client Side Win 10/944-WPF-Uriel/Demos/custom messageBox/Wpf_Windows/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Wpf_Windows
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            Window1 w = new Window1();
+            Window2 w2 = new Window2(12);
+            w.Show();
+            w2.Show();
+
+
+        }
+    }
+}
